51e5551b6af6022ab0d44c72daa29b68861adf22
Rahul <rahul25796@gmail.com>
2021-11-14 14:32:10 -0500

    initial commit

---
+++ src/main/java/com/module/app/multi/packager/PackagerApplication.java
@@ -0,0 +1,13 @@
+package com.module.app.multi.packager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PackagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PackagerApplication.class, args);
+	}
+
+}
